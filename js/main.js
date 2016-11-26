69467e1a605d33f836775d7cad10da0c1feaac11
Felixxp <xxp@adachina.net>
2016-11-26 21:45:16 +0800

    add gesture listerner

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 document.addEventListener('gest', function(gesture) {
     //handle gesture .direction .up .down .left .right .error
     if (gesture.left) {
+    	$("#current").html("left")
     	console.log("left")
+    	$(".gest-left i").html($(".gest-left i")-0+1)
+    }
+    if (gesture.right) {
+    	$("#current").html("right")
+    	console.log("right")
+    	$(".gest-right i").html($(".gest-right i")-0+1)
+    }
+    if (gesture.up) {
+    	$("#current").html("right")
+    	console.log("up");
+    	$(".gest-up i").html($(".gest-up i")-0+1)
+    }
+    if (gesture.down) {
+    	$("#current").html("right");
+    	console.log("down");
+    	$(".gest-down i").html($(".gest-down i")-0+1)
     }
 }, false);
 
